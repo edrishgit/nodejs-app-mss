d5195590b2cce6d045144fb57fe2e36c96b4ced3
edrishgit <awsedrish@gmail.com>
2026-01-21 20:25:40 +0530

    Update SonarQube authentication method
    Replaced sonar.login with sonar.token for authentication.

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'sqa_5a659a771d99e1a5d696be40c1ad04646acf4df4',
+	    'sonar.token': 'sqa_0f0deeff98037f3b099cb4a0770f7f9d6ff02fad',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
